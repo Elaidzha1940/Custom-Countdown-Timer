ee246c2a5325771c58d2a619f0ff6c4886009186
Elaidzha1940 <eli080293@gmail.com>
2023-05-29 19:32:23 +0700

    Custom Countdown Timer + added a New File / Content-ViewModel

--- Custom Countdown Timer/Custom Countdown Timer/Content-VeiwModel.swift
+++ Custom Countdown Timer/Custom Countdown Timer/Content-VeiwModel.swift
@@ -10,3 +10,24 @@
 
 import Foundation
 
+extension ContentView {
+    final class ViewMoel: ObservableObject {
+        
+        @Published var isActive = false
+        @Published var showingAlert = false
+        @Published var time: String = "5:00"
+        @Published var minutes: Float = 5.0 {
+            didSet {
+                self.time = "\(Int(minutes)):00"
+            }
+        }
+        
+        private var initialTime = 0
+        private var endDate = Date()
+        
+        func start(minutes: Float) {
+            self.initialTime = Int(minutes)
+            
+        }
+    }
+}
