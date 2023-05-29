bf94bf706466c161a7bb6deed6c77839f47cbe25
Elaidzha1940 <eli080293@gmail.com>
2023-05-29 19:21:26 +0700

    Custom Countdown Timer

---
+++ Custom Countdown Timer/Custom Countdown Timer/Custom_Countdown_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Countdown_TimerApp.swift
+//  Custom Countdown Timer
+//
+//  Created by Elaidzha Shchukin on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Countdown_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
